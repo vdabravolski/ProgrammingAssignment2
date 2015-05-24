465902f07125118f95d81947c231d4128d7e22e5
Vadim Dobrovolsky <vadim.dobrovolsky@gmail.com>
2015-05-24 11:55:51 -0400

    Adding working and commented assignment code to the repo

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## makeCacheMatrix function returns the list of functions "set", "get", "setinverse" and "getinverse".
+## These functions manipulate with variables "x" (original matrix) and "m" (inverted matrix).
+## Variable "m" is cache variable and either contains NULL (cache is empty) or inverted matrix (cache exists and is valid).
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  
+  set <- function(y) { # function to set new value for 
+    x <<- y
+    m <<- NULL #nulling the matrix parameters since we want to recalculate it
+  }
+  
+  get <- function() x
+  
+  getinverse <- function() m
+  
+  setinverse <- function(z){
+    m <<- z
+  }
+  
+  list(set=set, get=get, getinverse=getinverse, setinverse=setinverse)
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve returns the inverted matrix. 
+## If cache is not empty than it simpy returns cache (variable "m"). 
+## If cache is empty than it calculates the inverted martix and refreshes the cache (variable "m")
+## Input parameters: function makeCacheMatrix
+## Output: inverted matrix (variable "m")
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if (!is.null(m)){ # check if cache ("m" variable) is empty or not.
+    message("getting cached data")
+    return(m) # return current cache
+  }
+  
+  data <-x$get()
+  m <- solve(data) # since cahce is empty, recalculate the cache.
+  x$setinverse(m)
+  m
 }
+
+
